5c9cd91f74387863b56ab783a836d5a96797a534
ttoha <toooxa@gmail.com>
2017-01-16 02:12:55 +0200

    fix responsive

--- js/jquery.tslider.js
+++ js/jquery.tslider.js
@@ -2,43 +2,48 @@
     $.fn.tslider = function(options){
         var defaults = {
             slide_name: true,   // false - отключит название слайдов на стрелках прокрутки
-            autoplay: 3000,     // интервал между прокруткой слайда 0 или false - отключит авто прокрутку
+            autoplay: 3000,     // интервал между прокруткой слайда, 0 - отключит авто прокрутку
             speed: 500          // скорость прокрутки
         }
 
         var settings = $.extend(defaults, options);
 
         var ts = this;
+        var slide_width;
+        var slider_width;
 
-        var slide_width = ts.find('.slider-wrap').outerWidth();
-        var slider_width = ts.find('.slider .slide').length * slide_width;
-        ts.find('.slider').css({"width": + slider_width + "px"});
-        ts.find('.slider .slide').css({"width": + slide_width + "px"});
+        function apply_resize(){
+          slide_width = ts.find('.slider-wrap').outerWidth();
+          slider_width = ts.find('.slider .slide').length * slide_width;
 
+          ts.find('.slider').css({"width": + slider_width + "px"});
+          ts.find('.slider .slide').css({"width": + slide_width + "px"});
+
+          ts.find('.slider .slide').css({"height": "auto"});
+          slide_height = ts.find('.slider').outerHeight();
+          ts.find('.slider .slide').css({"height": + slide_height + "px"});
+        }
+
+        apply_resize();
+
+        $(window).resize(apply_resize);
 
         function slide_name(){
             if (settings.slide_name == true){
                 var left_text = $(ts).find(".slider .slide").last().find(".slide-name").text();
                 var right_text = $(ts).find(".slider .slide").eq(1).find(".slide-name").text();
-                ts.find(".slider-left_arrow").html(left_text);
-                ts.find(".slider-right_arrow").html(right_text);
+                ts.find(".slider-left_arrow .text").html(left_text);
+                ts.find(".slider-right_arrow .text").html(right_text);
             }
         }
 
         slide_name();
 
-        $(window).resize(function() {
-            var slide_width = ts.find('.slider-wrap').outerWidth();
-            var slider_width = ts.find('.slider .slide').length * slide_width;
-            ts.find('.slider').css({"width": + slider_width + "px"});
-            ts.find('.slider .slide').css({"width": + slide_width + "px"});
-        });
-
-        $(".slider-left_arrow").on('click', function(){
+        $(ts).find(".slider-left_arrow").on('click', function(){
             slider_left();
             return false;
         });
-        $(".slider-right_arrow").on('click', function(){
+        $(ts).find(".slider-right_arrow").on('click', function(){
             slider_right();
             return false;
         });
